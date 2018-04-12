ea8be798a26d6bfb48c253c56f6195cbb443ad5b
tzinyama <tzinyama@colgate.edu>
2018-04-12 17:48:48 -0400

    disbled all controller before_action; updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,41 @@
 Rails.application.routes.draw do
 
-  get 'meetings/index'
+  # get 'meetings/index'
+  #
+  # get 'meetings/show'
+  #
+  # get 'meetings/new'
+  #
+  # get 'meetings/create'
+  #
+  # get 'meetings/edit'
+  #
+  # get 'meetings/update'
+  #
+  # get 'meetings/destroy'
+  #
+  # get 'meetings/about'
+  #
+  # get 'people/index'
+  #
+  # get 'people/show'
+  #
+  # get 'people/new'
+  #
+  # get 'people/create'
+  #
+  # get 'people/edit'
+  #
+  # get 'people/update'
+  #
+  # get 'people/destroy'
+  #
+  # get 'people/about'
 
-  get 'meetings/show'
-
-  get 'meetings/new'
-
-  get 'meetings/create'
-
-  get 'meetings/edit'
-
-  get 'meetings/update'
-
-  get 'meetings/destroy'
-
-  get 'meetings/about'
-
-  get 'people/index'
-
-  get 'people/show'
-
-  get 'people/new'
-
-  get 'people/create'
-
-  get 'people/edit'
-
-  get 'people/update'
-
-  get 'people/destroy'
-
-  get 'people/about'
-
-  # get "/about" => 'events#about'
+  get "/about" => 'events#about'
   # get "/all" => 'events#all'
   # get "/leaderboard" => 'users#leaderboard'
-  # get "/contact" => 'users#contact'
+  get "/contact" => 'users#contact'
 
   # devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   # devise_scope :user do
@@ -49,6 +49,7 @@ Rails.application.routes.draw do
   resources :people
   resources :meetings
   root to: 'main#general'
+  #root to: 'meetings#index'
 
   # resources :events do
   #   resources :registrees
