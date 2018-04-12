ffdc7485d7fc08b4ed2dddc0f0f2d30278e5bf1a
tzinyama <tzinyama@colgate.edu>
2018-04-12 19:57:55 -0400

    meetings edit now working

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -13,9 +13,21 @@ class MeetingsController < ApplicationController
   end
 
   def create
+    @meeting = Meeting.new(create_update_params)
+
+    if @meeting.save
+      flash[:notice] = "New event \'#{@meeting.title}\' created"
+      redirect_to meetings_path and return
+    else
+      flash[:error] = "Error adding event"
+      redirect_to new_meeting_path(@meeting) and return
+    end
+    #render :action => 'new'
   end
 
   def edit
+    id = params[:id]
+    @meeting = Meeting.find(id)
   end
 
   def update
@@ -25,5 +37,10 @@ class MeetingsController < ApplicationController
   end
 
   def about
+  end
+
+  private
+  def create_update_params
+    params.require(:meeting).permit(:title, :location, :location, :description, :points, :image)
   end
 end
