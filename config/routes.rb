99fd27a9f45d636393c56730f2a5b9b66b3747c2
tzinyama <tzinyama@colgate.edu>
2018-04-12 17:16:06 -0400

    updated db scheme: created person and meeting controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,60 @@
 Rails.application.routes.draw do
 
-  get "/about" => 'events#about'
-  get "/all" => 'events#all'
-  get "/leaderboard" => 'users#leaderboard'
-  get "/contact" => 'users#contact'
+  get 'meeting/index'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
-  devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
+  get 'meeting/show'
 
-  resources :users
-  resources :events
+  get 'meeting/new'
+
+  get 'meeting/create'
+
+  get 'meeting/edit'
+
+  get 'meeting/update'
+
+  get 'meeting/destroy'
+
+  get 'meeting/about'
+
+  get 'person/index'
+
+  get 'person/show'
+
+  get 'person/new'
+
+  get 'person/create'
+
+  get 'person/edit'
+
+  get 'person/update'
+
+  get 'person/destroy'
+
+  get 'person/about'
+
+  # get "/about" => 'events#about'
+  # get "/all" => 'events#all'
+  # get "/leaderboard" => 'users#leaderboard'
+  # get "/contact" => 'users#contact'
+
+  # devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  # devise_scope :user do
+  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
+
+  # resources :users
+  # resources :events
+  # root to: 'main#general'
+
+  resources :people
+  resources :meetings
   root to: 'main#general'
 
-  resources :events do
-    resources :registrees
-  end
-  resources :occurrences do
-    resources :attendees
-  end
+  # resources :events do
+  #   resources :registrees
+  # end
+  # resources :occurrences do
+  #   resources :attendees
+  # end
 
 end
